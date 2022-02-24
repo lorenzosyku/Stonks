f1b229f30787c361b0ac0876f590c55ceebc5e9e
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-24 14:22:47 -0500

    added animations to the sections

--- src/LandingPageComponents/LandingSecOne.js
+++ src/LandingPageComponents/LandingSecOne.js
@@ -3,6 +3,8 @@ import { useEffect, useRef } from "react";
 
 function LandingSecOne() {
   const container = useRef(null);
+  const pie = useRef(null);
+  const bars = useRef(null); 
 
   useEffect(() => {
     lottie.loadAnimation({
@@ -12,41 +14,66 @@ function LandingSecOne() {
       autoplay: true,
       animationData: require("./working.json"),
     });
+    lottie.loadAnimation({
+      container: pie.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./report-animated-icon.json"),
+    });
+    lottie.loadAnimation({
+      container: bars.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./investment-chart.json"),
+    });
   }, []);
 
   return (
-    <section className="min-h-screen bg-shade-darkgrayblue  ">
-      <div className="py-10 flex flex-col justify-center items-center md:grid grid-rows-3 grid-flow-col gap-4">
-        <div className="md:row-span-3 ">
-          <div ref={container}></div>
+    <section className="min-h-screen bg-shade-lightblue flex items-center">
+      <div className="md:flex flex-col justify-center items-center py-12 space-y-5">
+        <div className="flex justify-between px-12">
+          <div className="hidden md:flex w-1/4">
+            <div className=""></div>
+            <div ref={container}></div>
+          </div>
+          <div className="md:w-1/2 flex items-center md:px-5 border">
+            <p className="text-slate-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              quidem non harum consectetur sed voluptates dolore pariatur eaque
+              repudiandae. Tenetur, qui quos aperiam aut ratione vel excepturi
+              rem nulla culpa
+            </p>
+          </div>
         </div>
-        <div className="w-1/2 md:col-span-2 px-5 border mx-5">
-          <div className=""></div>
-          <p className="text-slate-200">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-            quidem non harum consectetur sed voluptates dolore pariatur eaque
-            repudiandae. Tenetur, qui quos aperiam aut ratione vel excepturi rem
-            nulla culpa?
-          </p>
+        <div className="flex justify-between px-12">
+          <div className="md:w-1/2 flex items-center md:px-5 border">
+            <div className=""></div>
+            <p className="text-slate-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              quidem non harum consectetur sed voluptates dolore pariatur eaque
+              repudiandae. Tadklslsllssssm aut ratione vel excepturi rem nulla culpa
+            </p>
+          </div>
+          <div className="hidden md:flex w-1/4">
+            <div className=""></div>
+            <div ref={pie}></div>
+          </div>
         </div>
-        <div className="w-1/2 md:col-span-2 px-5 border mx-5">
-          <div className=""></div>
-          <p className="text-slate-200">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-            quidem non harum consectetur sed voluptates dolore pariatur eaque
-            repudiandae. Tenetur, qui quos aperiam aut ratione vel excepturi rem
-            nulla culpa?
-          </p>
-        </div>
-
-        <div className="w-1/2 md:col-span-2 px-5 border mx-5">
-          <div className=""></div>
-          <p className="text-slate-200">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-            quidem non harum consectetur sed voluptates dolore pariatur eaque
-            repudiandae. Tenetur, qui quos aperiam aut ratione vel excepturi rem
-            nulla culpa?
-          </p>
+        <div className="flex justify-between px-12">
+          <div className="hidden md:flex w-1/4">
+            <div className=""></div>
+            <div ref={bars}></div>
+          </div>
+          <div className="md:w-1/2 flex items-center md:px-5 border">
+            <p className="text-slate-200">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              quidem non harum consectetur sed voluptates dolore pariatur eaque
+              repudiandae. Tenetur, qui quos aperiam aut ratione vel excepturi
+              rem nulla culpa
+            </p>
+          </div>
         </div>
       </div>
     </section>
