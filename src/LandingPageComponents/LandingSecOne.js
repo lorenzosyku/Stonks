de2bcdbc03da3ab99bd049e43fbbe9a809b02cb7
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-27 10:38:46 -0500

    deleted unnecessary bits

--- src/LandingPageComponents/LandingSecOne.js
+++ src/LandingPageComponents/LandingSecOne.js
@@ -2,18 +2,10 @@ import lottie from "lottie-web";
 import { useEffect, useRef } from "react";
 
 function LandingSecOne() {
-  const container = useRef(null);
   const pie = useRef(null);
   const bars = useRef(null);
 
   useEffect(() => {
-    /*lottie.loadAnimation({
-      container: container.current,
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: require("./working.json"),
-    });*/
     lottie.loadAnimation({
       container: pie.current,
       renderer: "svg",
@@ -38,19 +30,6 @@ function LandingSecOne() {
           Analytics and data menagement
         </h1>
       </div>
-        {/*<div className="flex justify-between items-center px-12">
-          <div className="md:w-1/4">
-            <div ref={container}></div>
-          </div>
-          <div className="md:w-1/2 flex items-center md:px-5 border">
-            <p className="text-slate-200">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-              quidem non harum consectetur sed voluptates dolore pariatur eaque
-              repudiandae. Tenetur, qui quos aperiam aut ratione vel excepturi
-              rem nulla culpa
-            </p>
-          </div>
-        </div>*/}
         <div className="flex justify-between items-center px-12">
           <div className="md:w-1/2 flex items-center md:px-5 border">
             <p className="text-slate-200">
