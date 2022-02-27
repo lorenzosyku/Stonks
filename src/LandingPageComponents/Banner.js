6f9b112cad04a0e413ba9c063be4c687fd25946f
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-27 10:31:57 -0500

    made landing page more responsive

--- src/LandingPageComponents/Banner.js
+++ src/LandingPageComponents/Banner.js
@@ -3,30 +3,30 @@ function Banner() {
   
   return (
     <section id="about" className="min-h-screen bg-pack-train">
-      <div className="md:flex ">
-        <div className="max-w-4xl mx-auto text-white p-10 pr-24">
-          <h1 className="text-6xl max-w-xl mx-auto font-poppins font-semibold pt-5">
+      <div className="min-h-screen flex items-center">
+        <div className="max-w-4xl mx-auto text-white p-10 md:pr-24">
+          <h1 className="text-5xl md:text-6xl max-w-xl mx-auto font-poppins font-semibold pt-5">
             Test your ability to trade and uderstand the market
           </h1>
           <br />
           <br />
           <br />
-          <h2 className="text-lg max-w-xl mx-auto font-poppins">
+          <h2 className="text-md md:text-lg max-w-xl mx-auto font-poppins">
             Looking for a real chance at learning how to trade through your own
             trial and error, but you can not afford to loose money? Do you want
             to test different{" "}
-            <span className="text-shade-lightblue font-semibold italic text-lg">
+            <span className="text-shade-lightblue text-md font-semibold italic md:text-lg">
               trading strategies
             </span>{" "}
             without worrying about the{" "}
-            <span className="text-amber-400 font-semibold italic text-lg">
+            <span className="text-amber-400 text-md font-semibold italic md:text-lg">
               potential mistakes
             </span>{" "}
             you might do along the way?
           </h2>
 
-          <p className="text-lg py-5 max-w-xl mx-auto font-poppins">
-            <span className="text-2xl font-semibold italic text-shade-lightblue">TestNet-V1 </span> is
+          <p className="text-md md:text-lg py-5 max-w-xl mx-auto font-poppins">
+            <span className="text-lg md:text-2xl font-semibold italic text-shade-lightblue">TestNet-V1 </span> is
             a platform designed for inexpirienced and experienced traders that would like to learn how to approach the markets more responsively
           </p>
         </div>
