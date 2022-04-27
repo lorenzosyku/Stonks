bc5b00e18678ee570f467d9bba8a81f8e5b54ff2
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-04-27 09:12:48 -0400

    small fixes

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -72,23 +72,6 @@ function TransactionList({ dbTnxs }) {
                       >
                         <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                           <div className="flex space-x-3 items-center">
-                            <div
-                              //onClick={deleteBoughtTnx}
-                              className="bg-slate-800 p-2 rounded-md cursor-pointer"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-3 w-3"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                              >
-                                <path
-                                  fill-rule="evenodd"
-                                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                  clip-rule="evenodd"
-                                />
-                              </svg>
-                            </div>
                             <div>{rowData.stockName}</div>
                           </div>
                         </td>
@@ -148,23 +131,6 @@ function TransactionList({ dbTnxs }) {
                       >
                         <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                           <div className="flex space-x-3 items-center">
-                            <div
-                              //onClick={deleteSoldTnx}
-                              className="bg-slate-800 p-2 rounded-md cursor-pointer"
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-3 w-3"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                              >
-                                <path
-                                  fill-rule="evenodd"
-                                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                  clip-rule="evenodd"
-                                />
-                              </svg>
-                            </div>
                             <div>{rowData.stockName}</div>
                           </div>
                         </td>
