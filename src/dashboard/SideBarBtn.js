cf05565745a38bb703efab3158747a6ecd8d5534
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-06-09 21:49:56 -0400

    small fix

--- src/dashboard/SideBarBtn.js
+++ src/dashboard/SideBarBtn.js
@@ -2,14 +2,14 @@ import { MenuIcon } from "@heroicons/react/solid";
 
 function SideBarBtn({ setIsSidebarOpen, isSidebarOpen }) {
   return (
-    <div className="w-1/8 bg-slate-800">
+    <div className="w-1/8">
       <button
         onClick={() => {
           setIsSidebarOpen(true);
         }}
         className={`${
           isSidebarOpen ? "-translate-x-48" : "translate-x-0"
-        } top-8 transition transform ease-linear duration-500 text-gray-600 w-8 h-8 rounded-full flex items-center justify-center active:bg-gray-300 focus:outline-none ml-6 hover:bg-gray-200 hover:text-gray-800`}
+        } top-4 mt-4 transition transform ease-linear duration-500 text-gray-600 w-8 h-8 rounded-full flex items-center justify-center active:bg-gray-300 focus:outline-none ml-6 hover:bg-gray-200 hover:text-gray-800`}
       >
         <MenuIcon className="h-5 w-5 " />
       </button>
