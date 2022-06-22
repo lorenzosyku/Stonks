0bcb67a761c04b8cc8d34e16b28f1c9218326d21
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-06-22 00:05:02 -0400

    reformatted component

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -5,12 +5,40 @@ function PortfolioItem({stock, shares, percentage, prev, curr}) {
     percentage(prev, curr)
   }, [curr])
   let change = percentage(prev, curr)
+  let tot = shares*curr
   return (
-    <div className="grid grid-cols-3 bg-gray-200 rounded-md my-2 p-1">
+    // <div className="grid grid-cols-3 bg-gray-200 rounded-md my-2 p-1">
       
-      <h2>{stock}</h2>
-      <h3 className="flex justify-center">{shares}</h3>
-      <h3 className="flex justify-end">{change.toFixed(2)}%</h3>
+    //   <h2>{stock}</h2>
+    //   <h3 className="flex justify-center">{shares}</h3>
+    //   <h3 className="flex justify-end">{change.toFixed(2)}%</h3>
+    //   <h3>{curr.toFixed(2)}</h3>
+    // </div>
+    <div className="flex bg-slate-50">
+      <div className="bg-slate-200 flex justify-center items-center">👍</div>
+      <div className="flex flex-col flex-grow space-y-3">
+        <div className="flex justify-between items-center px-5 pt-2">
+          <div>
+            <p className="font-semibold">{stock}</p>
+            <p className="text-sm text-stone-500">{curr.toFixed(2)}</p>
+          </div>
+          <div>
+            <p className="font-semibold">{shares}</p>
+            <p className="text-sm text-stone-500">$ {tot.toFixed(2)}</p>
+          </div>
+        </div>
+        <div className="px-5 pb-2">
+          <div className="w-full bg-gray-200 rounded-full">
+            <div
+              className="bg-blue-600 text-xs w-1/4 font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+              
+            >
+              {" "}
+              25%
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/dashboard/PortfolioSection.js
+++ src/dashboard/PortfolioSection.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Balance from "../components/Balance";
 import PortfolioGraph from "../components/PortfolioGraph";
 import TotalReturnsGraph from "../components/TotalReturnsGraph";
-import AssetAllocation from "./AssetAllocation";
+
 
 function PortfolioSection({
   isSidebarOpen,
@@ -22,21 +22,16 @@ function PortfolioSection({
         totto={totto}
       />
       <div className="md:flex md:items-center md:justify-center">
-        <PortfolioGraph
+        {/* <PortfolioGraph
           dbPortfolio={dbPortfolio}
           setTotto={setTotto}
           totto={totto}
-        />
+        /> */}
         <div className="w-3/5 bg-gray-100 p-5 rounded-md m-5">
           <Balance dbPortfolio={dbPortfolio} />
         </div>
       </div>
-      <div className="">
-        <h2>Asset Allocation</h2>
-        <div className="p-3">
-          <AssetAllocation />
-        </div>
-      </div>
+      
     </div>
   );
 }
